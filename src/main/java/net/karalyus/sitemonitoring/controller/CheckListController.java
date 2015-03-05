0eb010c7ac1ecc2b72c839587e54a33d4cfdfc64
Simon <Simon@WIN-I1V1QNUULGN>
2015-03-05 23:51:51 +0200

    JSF & PrimeFaces & Spring tutorial 18: PrimeFaces Dialog

--- src/main/java/net/karalyus/sitemonitoring/controller/CheckListController.java
+++ src/main/java/net/karalyus/sitemonitoring/controller/CheckListController.java
@@ -47,4 +47,9 @@ public class CheckListController implements Serializable{
 		FacesContext.getCurrentInstance().
 			addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Check removed!", null));
 	}
+	
+	
+	public void clear(){
+		check = new Check();
+	}
 }
